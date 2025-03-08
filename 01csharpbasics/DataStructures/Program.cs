a002e3072d7b4e2d036d77cef2e4eb8d4fb85695
Michael Egbinola <michaelmarketing25@gmail.com>
2025-03-08 10:47:16 -0600

    learning the basics of c#

---
+++ 01csharpbasics/DataStructures/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DataStructure
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Data Structures");
+        }
+    }
+
+}
+
+
