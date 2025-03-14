6f083d7aa22a76421f41fa1e0d8ab5335d1615a3
Michael Egbinola <michaelmarketing25@gmail.com>
2025-03-14 05:46:50 -0500

    used loops - for, foreach, while, dowhile

--- 01csharpbasics/ConditionNLoops/Program.cs
+++ 01csharpbasics/ConditionNLoops/Program.cs
@@ -4,6 +4,13 @@ public class Program
 {
     static void Main(string[] args)
     {
+        // Quicly split a string
+        string names = "Bisi, Pupo, Olami, Sam, Abike";
+        string[] splittedName = names.Split(", ");
+        Console.WriteLine($"My name is {splittedName[0]}");
+        Console.WriteLine($"My name is {splittedName[1]}");
+        //Console.WriteLine($"My name is {splittedName[2]}");
+        //Console.WriteLine($"My name is {String.Join(",", splittedName[3..])}\n");
 
         Console.WriteLine("Conditional Statements ...\n");
         // Conditional Statements
@@ -54,6 +61,93 @@ public class Program
         }
 
         Console.WriteLine("\nLoops....\n");
+
+        // using foreach
+
+        //foreach (string item in splittedName)
+        //{
+        //    Console.WriteLine(item);
+        //}
+
+        // for loops
+
+        for (int i = 0; i < splittedName.Length; i++)
+        {
+            Console.WriteLine(splittedName[i]);
+
+        }
+
+        // print even numbers from 1-20
+        //for (int i = 1; i <= 10; i++)
+        //{
+        //    string type = (i % 2 == 0) ? "Even Number" : "Odd Number";
+        //    Console.WriteLine($"{type} : {i}");
+        //}
+
+        // for loop
+
+        int[] n = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+        int count = 0;
+
+        double startTime = DateTime.Now.TimeOfDay.TotalSeconds;
+
+        for (int i = 0; i < n.Length; i++)
+        {
+            count += n[i];
+        }
+
+        double endTime = DateTime.Now.TimeOfDay.TotalSeconds;
+        Console.WriteLine($"Elapsed Time using for loop: {endTime - startTime}");
+        Console.WriteLine("Sum: " + count);
+
+        // foreach
+
+        int sum = 0;
+        double startTime1 = DateTime.Now.TimeOfDay.TotalSeconds;
+
+        foreach (int i in n)
+        {
+            sum += i;
+        }
+
+        double endTime1 = DateTime.Now.TimeOfDay.TotalSeconds;
+        Console.WriteLine($"Elapsed Time using foreach: {endTime1 - startTime1}"); 
+        Console.WriteLine("Sum: " + sum);
+
+        // while loop
+        int iWhile = 0;
+        int xSum = 0;
+
+        while (iWhile < n.Length)
+        {
+            xSum += n[iWhile];
+            iWhile++;
+        }
+
+        Console.WriteLine($"XSum: {xSum}");
+
+        // do while loop
+        int iDoWhile = 0;
+        int doSum = 0;
+
+        do
+        {
+            doSum += iDoWhile;
+            iDoWhile++; // stop at length-1
+        }
+
+        while (iDoWhile <= n.Length);
+
+        Console.WriteLine($"DoSUM: {doSum}");
+
+
+
+
+
+
+
+
 
 
     }
